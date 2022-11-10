7fce6c1727cdb3578ce17976ede648e7c60d030e
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-10 16:08:35 -0500

    Switch

--- app/src/main/java/com/sgcp/fourth_app_test/MainActivity.kt
+++ app/src/main/java/com/sgcp/fourth_app_test/MainActivity.kt
@@ -3,11 +3,18 @@ package com.sgcp.fourth_app_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Switch
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val swEncendido: Switch = findViewById(R.id.sw_encender);
+        swEncendido.setOnCheckedChangeListener({ _ , isChecked ->
+            val texto = if ( isChecked ) "Encendido" else "Apagado";
+            swEncendido.text = texto;
+        })
     }
 }
