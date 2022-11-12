a92b5507ee16a9e49124ba8be873c9462657e5a6
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-12 09:17:07 -0500

    Crear Inicio de Sesión

--- app/src/main/java/com/sgcp/fourth_app_test/MainActivity.kt
+++ app/src/main/java/com/sgcp/fourth_app_test/MainActivity.kt
@@ -2,14 +2,28 @@ package com.sgcp.fourth_app_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.Switch
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val etUsuario: EditText = findViewById(R.id.et_user);
+        val etPassword: EditText = findViewById(R.id.et_pass);
+        val btnIniciar: Button = findViewById(R.id.btn_iniciar_sesion);
+
+    }
+
+    fun iniciarSesion(view: View) {
+        val etUsuario: EditText = findViewById(R.id.et_user);
+        val etPassword: EditText = findViewById(R.id.et_pass);
+
+        if( (etUsuario.text.toString() == "Sleyther") && (etPassword.text.toString() == "1234")) {
+            Toast.makeText(this, "Iniciando sesión...", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "usuario o contraseña incorrectos...", Toast.LENGTH_LONG).show();
+        }
     }
 }
