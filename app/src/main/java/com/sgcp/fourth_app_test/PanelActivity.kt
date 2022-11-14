4a7f743f2196a14615b13dafc37c274cf38412c4
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-14 17:44:43 -0500

    Crear Actividad Panel

---
+++ app/src/main/java/com/sgcp/fourth_app_test/PanelActivity.kt
@@ -0,0 +1,11 @@
+package com.sgcp.fourth_app_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PanelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_panel)
+    }
+}
