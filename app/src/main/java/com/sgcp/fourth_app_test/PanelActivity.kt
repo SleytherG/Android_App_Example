56036ce7379f5e118a42ae89d8c0c9510d5e3734
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-15 10:13:52 -0500

    Agregar Fragment a una Actividad

--- app/src/main/java/com/sgcp/fourth_app_test/PanelActivity.kt
+++ app/src/main/java/com/sgcp/fourth_app_test/PanelActivity.kt
@@ -2,10 +2,16 @@ package com.sgcp.fourth_app_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class PanelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_panel)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<PrincipalFragment>(R.id.fcv_main_container)
+        }
     }
 }
