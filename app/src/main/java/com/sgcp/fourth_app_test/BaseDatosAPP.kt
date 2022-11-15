32e368e8e6c71debc03251904fae68b0ecf3fe74
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-15 10:59:47 -0500

    Creación Clase BaseDatosAPP

---
+++ app/src/main/java/com/sgcp/fourth_app_test/BaseDatosAPP.kt
@@ -0,0 +1,17 @@
+package com.sgcp.fourth_app_test
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BaseDatosAPP(context: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?, version: Int ):
+    SQLiteOpenHelper(context, name, factory, version) {
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+}
