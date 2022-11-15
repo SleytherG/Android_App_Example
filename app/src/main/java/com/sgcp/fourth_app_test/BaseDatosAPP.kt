40ead73d52aa062f6fefe6bb833caa379e790796
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-15 11:21:48 -0500

    Creando Tabla usuarios

--- app/src/main/java/com/sgcp/fourth_app_test/BaseDatosAPP.kt
+++ app/src/main/java/com/sgcp/fourth_app_test/BaseDatosAPP.kt
@@ -7,8 +7,14 @@ import android.database.sqlite.SQLiteOpenHelper
 class BaseDatosAPP(context: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?, version: Int ):
     SQLiteOpenHelper(context, name, factory, version) {
 
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+    val create_users_table = "CREATE TABLE Usuarios" +
+            "(ID INT PRIMARY KEY," +
+            "NOMBRE TEXT," +
+            "PASSWORD TEXT)";
+//    val insert_data_table_users = "INSERT INTO Usuarios (ID, NOMBRE, PASSWORD) VALUES (1, 'Sleyther', '1234')";
+
+    override fun onCreate(database: SQLiteDatabase?) {
+        database?.execSQL(create_users_table);
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
