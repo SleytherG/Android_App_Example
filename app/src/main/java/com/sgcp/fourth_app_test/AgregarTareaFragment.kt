0fe6c834b8ba6778f872b2ecfcbcb2d25d3e33ba
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-15 18:23:34 -0500

    Obtener imagen de galeria

--- app/src/main/java/com/sgcp/fourth_app_test/AgregarTareaFragment.kt
+++ app/src/main/java/com/sgcp/fourth_app_test/AgregarTareaFragment.kt
@@ -1,10 +1,17 @@
 package com.sgcp.fourth_app_test
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +28,10 @@ class AgregarTareaFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private val pickImage = 0;
+    private var imageUri: Uri? = null;
+    lateinit var imgCargada: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,8 +45,28 @@ class AgregarTareaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar_tarea, container, false)
+
+        val rootView = inflater.inflate(R.layout.fragment_agregar_tarea, container, false);
+
+        imgCargada = rootView.findViewById(R.id.iv_imagen_cargada);
+        val btnSeleccionarImagen: Button = rootView.findViewById(R.id.btn_img_tarea);
+        btnSeleccionarImagen.setOnClickListener {
+            val galeria = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            startActivityForResult(galeria, pickImage);
+        }
+
+        return rootView;
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if ( resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data;
+            imgCargada.setImageURI(imageUri);
+        }
+    }
+
 
     companion object {
         /**
