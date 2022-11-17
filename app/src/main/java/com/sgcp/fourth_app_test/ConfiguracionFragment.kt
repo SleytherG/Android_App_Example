0d0c3fb6c605577ebdd5f3f5b33af21b18138608
SleythergCP <sleyther.calsin@cloudcomputing.com.pe>
2022-11-17 15:52:14 -0500

    Agregar fuente personalizada

--- app/src/main/java/com/sgcp/fourth_app_test/ConfiguracionFragment.kt
+++ app/src/main/java/com/sgcp/fourth_app_test/ConfiguracionFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,12 @@ class ConfiguracionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracion, container, false)
+        val rootView: View = inflater.inflate(R.layout.fragment_configuracion, container, false);
+        rootView.findViewById<TextView?>(R.id.tv_rights).typeface =
+            context?.let {
+                ResourcesCompat.getFont(it, R.font.supercell_magic)
+        }
+        return rootView;
     }
 
     companion object {
